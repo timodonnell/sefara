893001e4d30141a1ce4b6352e0f01dbc1dd97c86
Tim O'Donnell <timodonnell@gmail.com>
2015-06-12 18:03:57 -0400

    add travis

---
+++ deploy-gh-pages.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Taken from https://github.com/w3ctag/promises-guide/blob/master/deploy-gh-pages.sh
+
+cd docs
+make clean
+make setup
+make rst
+make html
+cd _build
+
+touch .nojekyll
+
+git init
+git config user.name "Travis-CI"
+git config user.email "travis@w3ctag.org"
+git add .
+git commit -m "Deploy to GitHub Pages"
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
